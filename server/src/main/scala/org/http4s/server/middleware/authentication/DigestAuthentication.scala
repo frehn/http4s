d9371255e9db4f6c43a7b10763298204f6e153f6
Bryce Anderson <bryce.anderson22@gmail.com>
2015-05-07 08:39:14 -0400

    Use java.util.LinkedHashMap to store nonces
    Using this data structure we can avoid iterating through the whole
    collection of nonces when doing a cleanup because we know they will
    expire in the order they were inserted into the list.

--- server/src/main/scala/org/http4s/server/middleware/authentication/DigestAuthentication.scala
+++ server/src/main/scala/org/http4s/server/middleware/authentication/DigestAuthentication.scala
@@ -6,8 +6,9 @@ package authentication
 import java.security.SecureRandom
 import java.math.BigInteger
 import java.util.Date
+
 import org.http4s.headers.Authorization
-import scala.collection.mutable.HashMap
+
 import scala.concurrent.duration._
 
 import scalaz.concurrent.Task
@@ -102,80 +103,4 @@ private[authentication] object Nonce {
   def gen(bits: Int) = {
     new Nonce(new Date(), 0, getRandomData(bits))
   }
-}
-
-private[authentication] object NonceKeeper {
-
-  sealed abstract class Reply
-
-  case object StaleReply extends Reply
-
-  case object OKReply extends Reply
-
-  case object BadNCReply extends Reply
-
-}
-
-/**
- * A thread-safe class used to manage a database of nonces.
- *
- * @param staleTimeout Amount of time (in milliseconds) after which a nonce
- *                     is considered stale (i.e. not used for authentication
- *                     purposes anymore).
- * @param bits The number of random bits a nonce should consist of.
- */
-private[authentication] class NonceKeeper(staleTimeout: Long, nonceCleanupInterval: Long, bits: Int) {
-  require(bits > 0, "Please supply a positive integer for bits.")
-  private val nonces = new HashMap[String, Nonce]
-  private var lastCleanup = System.currentTimeMillis()
-
-  /**
-   * Removes nonces that are older than staleTimeout
-   */
-  private def checkStale() = {
-    val d = System.currentTimeMillis()
-    if (d - lastCleanup > nonceCleanupInterval) {
-      nonces.retain{ case (_,n) => staleTimeout > d - n.created.getTime() }
-      lastCleanup = d
-    }
-  }
-
-  /**
-   * Get a fresh nonce in form of a {@link String}.
-   * @return A fresh nonce.
-   */
-  def newNonce(): String = {
-    var n: Nonce = null
-    nonces.synchronized {
-      checkStale()
-      do {
-        n = Nonce.gen(bits)
-      } while (nonces.contains(n.data))
-      nonces += (n.data -> n)
-    }
-    n.data
-  }
-
-  /**
-   * Checks if the nonce {@link data} is known and the {@link nc} value is
-   * correct. If this is so, the nc value associated with the nonce is increased
-   * and the appropriate status is returned.
-   * @param data The nonce.
-   * @param nc The nonce counter.
-   * @return A reply indicating the status of (data, nc).
-   */
-  def receiveNonce(data: String, nc: Int): NonceKeeper.Reply =
-    nonces.synchronized {
-      checkStale()
-      nonces.get(data) match {
-        case None => NonceKeeper.StaleReply
-        case Some(n) => {
-          if (nc > n.nc) {
-            n.nc = n.nc + 1
-            NonceKeeper.OKReply
-          } else
-            NonceKeeper.BadNCReply
-        }
-      }
-    }
 }
